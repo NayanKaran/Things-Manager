dd6e45963899fb4a7185fc18eb9f5c3f9accaef3
Debabrata Mandal <bapidebabrata@gmail.com>
2022-09-06 18:43:02 +0530

    Update tests for updated Book class

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -2,7 +2,7 @@ require_relative '../book'
 
 describe Book do
     before(:all) do
-        @book = Book.new('publisher', 'bad', '2020-01-01')
+        @book = Book.new('publisher', 'bad', '2020-01-01', 'title')
     end
     it 'should be an instance of Book' do
         expect(@book).to be_an_instance_of(Book)
@@ -21,21 +21,21 @@ describe Book do
     end
     describe '#can_be_archived?' do
         it 'should return true if the book is older than 10 years' do
-            @book = Book.new('publisher', 'good', '2010-01-01')
+            @book = Book.new('publisher', 'good', '2010-01-01', 'title')
             expect(@book.send(:can_be_archived?)).to be(true)
         end
         it 'should return true if the book has a bad cover state' do
-            @book = Book.new('publisher', 'bad', '2020-01-01')
+            @book = Book.new('publisher', 'bad', '2020-01-01', 'title')
             expect(@book.send(:can_be_archived?)).to be(true)
         end
         it 'should return false if the book is younger than 10 years and has a good cover state' do
-            @book = Book.new('publisher', 'good', '2020-01-01')
+            @book = Book.new('publisher', 'good', '2020-01-01', 'title')
             expect(@book.send(:can_be_archived?)).to be(false)
         end
     end
     describe '#to_json' do
         it 'should return a json representation of the book' do
-            expect(@book.to_json).to eq("{\"id\":\"#{@book.id}\",\"publish_date\":\"2020-01-01\",\"archived\":false,\"publisher\":\"publisher\",\"cover_state\":\"bad\",\"label_id\":null}")
+            expect(@book.to_json).to eq("{\"id\":\"#{@book.id}\",\"title\":\"title\",\"publish_date\":\"2020-01-01\",\"archived\":false,\"publisher\":\"publisher\",\"cover_state\":\"bad\",\"label_id\":null}")
         end
     end
     describe '#move_to_archive' do
