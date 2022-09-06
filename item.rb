367bf63cc338df87d12902fa31e980b9bc009e46
silviatofana <silvia.tofana@gmail.com>
2022-09-06 14:39:23 +0300

    update

--- item.rb
+++ item.rb
@@ -1,36 +1,40 @@
-require 'securerandom'
+require 'date'
 
 class Item
-  attr_reader :label, :genre, :author, :publish_date, :archived, :id
+  attr_accessor :genre, :author, :source, :label
+  attr_reader :id, :publish_date, :archived
 
   def initialize(publish_date)
-    @id = SecureRandom.uuid
+    @id = Random.rand(1..1000)
     @publish_date = publish_date
     @archived = false
   end
 
-  def genre=(genre)
+  def can_be_archived?
+    Time.now.year - Date.parse(@publish_date).year > 10
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+
+  def add_genre(genre)
     @genre = genre
     genre.items << self unless genre.items.include?(self)
   end
 
-  def author=(author)
+  def add_author(author)
     @author = author
-    author.items << self unless author.items.include?(self)
+    author.items.push(self) unless author.items.include?(self)
   end
 
-  def label=(label)
-    @label = label
+  def add_source(source)
+    @source = source
     label.items << self unless label.items.include?(self)
   end
 
-  def can_be_archived?
-    @archived
+  def add_label(label)
+    @label = label
+    label.items << self unless label.items.include?(self)
   end
-
-  def move_to_archive
-    @archived = true
-  end
-
-  private :can_be_archived?
 end
