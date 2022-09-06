0ee6cc09908ad126337143ea3587f97b80a88538
Debabrata Mandal <bapidebabrata@gmail.com>
2022-09-06 14:09:41 +0530

    Define Book class and it's properties and methods

--- book.rb
+++ book.rb
@@ -1,9 +1,23 @@
-class Book
+require_relative 'item'
+
+class Book < Item
+  def initialize(publisher, cover_state, publish_date)
+    super(publish_date)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+  def can_be_archived?
+    super && @cover_state == 'bad'
+  end
   def to_json(*_args)
     {
-      label_id: @label.id,
       id: @id,
-      publish_date: @publish_date
+      publish_date: @publish_date,
+      archived: @archived,
+      publisher: @publisher,
+      cover_state: @cover_state,
+      label_id: @label.id
     }.to_json
   end
+  private :can_be_archived?
 end
