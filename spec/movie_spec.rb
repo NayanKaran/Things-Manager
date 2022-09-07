145b5c823c3dee5f68b385fabf00c25f93f29c06
silviatofana <silvia.tofana@gmail.com>
2022-09-07 23:09:08 +0300

    correct

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -3,23 +3,17 @@ require_relative '../lib/classes/movie'
 describe Movie do
   describe 'Create a new movie older than 10 years but not silet' do
     movie = Movie.new('2002-08-02', archived: false, silet: false)
-
-     end
+  end
 
   describe 'Create a new movie not older than 10 years but silet' do
     movie = Movie.new('2022-08-02', archived: false, silet: true)
-
-      end
+  end
 
   describe 'Create a new movie older than 10 yeard and silet' do
     movie = Movie.new('2002-08-02', archived: false, silet: true)
-
-   
   end
 
   describe 'Create a movie not older than 10 years and not silet' do
     movie = Movie.new('2022-08-02', archived: false, silet: false)
-
-    
-   end
+  end
 end
