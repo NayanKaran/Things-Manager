124302a1399e6aed66402bc4363cddb112874290
Debabrata Mandal <bapidebabrata@gmail.com>
2022-09-09 10:56:28 +0530

    Fix bugs at json_db.rb

--- json_db.rb
+++ json_db.rb
@@ -4,11 +4,13 @@ def save_books(books)
   JSON.dump(books, File.open('./data/books.json', 'w'))
 end
 
-def load_books(books, labels)
+def load_books(app)
   JSON.parse(File.read('./data/books.json')).each do |book|
-    books << Book.new(book['publisher'], book['cover_state'], book['publish_date'], book['title'], id: book['id'])
+    books << Book.new(book['publisher'], book['cover_state'], book['publish_date'], id: book['id'])
     books.last.move_to_archive if book['archived']
-    labels.find { |label| label.id == book['label_id'] }&.add_item(books.last)
+    app.labels.find { |label| label.id == book['label_id'] }&.add_item(books.last)
+    app.genres.find { |genre| genre.id == book['genre_id'] }&.add_item(books.last)
+    app.authors.find { |author| author.id == book['author_id'] }&.add_item(books.last)
   end
 rescue Errno::ENOENT
   puts 'No books found'
@@ -31,17 +33,15 @@ def save_music_albums(music_albums)
 end
 
 def load_music_albums(app)
-  # check if file exists guard clause
-  return unless File.exist?('./data/music_albums.json')
-
   JSON.parse(File.read('./data/music_albums.json')).each do |music_album|
-    # get the label with the id of the music album
-    label = app.labels.find { |l| l.id == music_album['label'] }
-    app.music_albums << MusicAlbum.new(label, music_album['genre'],
-                                       publish_date: music_album['publish_date'],
-                                       id: music_album['id'])
+    app.music_albums << MusicAlbum.new(music_album['on_spotify'], music_album['publish_date'], id: music_album['id'])
     app.music_albums.last.move_to_archive if music_album['archived']
+    app.labels.find { |label| label.id == music_album['label_id'] }&.add_item(app.music_albums.last)
+    app.genres.find { |genre| genre.id == music_album['genre_id'] }&.add_item(app.music_albums.last)
+    app.authors.find { |author| author.id == music_album['author_id'] }&.add_item(app.music_albums.last)
   end
+rescue Errno::ENOENT
+  puts 'No music albums found'
 end
 
 def save_genres(genres)
@@ -57,16 +57,48 @@ def load_genres(genres)
   end
 end
 
+def save_authors(authors)
+  JSON.dump(authors, File.open('./data/authors.json', 'w'))
+end
+
+def load_authors(authors)
+  JSON.parse(File.read('./data/authors.json')).each do |author|
+    authors << Author.new(author['first_name'], author['last_name'], id: author['id'])
+  end
+  rescue Errno::ENOENT
+    puts 'No authors found'
+end
+
+def save_games(games)
+  JSON.dump(games, File.open('./data/games.json', 'w'))
+end
+
+def load_games(app)
+  JSON.parse(File.read('./data/games.json')).each do |game|
+    app.games << Game.new(game['multiplayer'], game['last_played_at'], game['publish_date'], id: game['id'])
+    app.games.last.move_to_archive if game['archived']
+    app.labels.find { |label| label.id == game['label_id'] }&.add_item(app.games.last)
+    app.genres.find { |genre| genre.id == game['genre_id'] }&.add_item(app.games.last)
+    app.authors.find { |author| author.id == game['author_id'] }&.add_item(app.games.last)
+  end
+rescue Errno::ENOENT
+  puts 'No games found'
+end
+
 def save_data(app)
-  save_music_albums(app.music_albums)
   save_labels(app.labels)
-  save_books(app.books)
+  save_authors(app.authors)
   save_genres(app.genres)
+  save_books(app.books)
+  save_music_albums(app.music_albums)
+  save_games(app.games)
 end
 
 def load_data(app)
   load_labels(app.labels)
-  load_books(app.books, app.labels)
   load_genres(app.genres)
+  load_authors(app.authors)
+  load_books(app)
   load_music_albums(app)
+  load_games(app)
 end
